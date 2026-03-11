66becea89658538e0dc8a7afeb3a3da6f170ff1c
Leonardo Bragatti <pf1761@fiap.com.br>
2026-03-11 08:55:23 -0300

    Definindo o tipo do usuario

--- index.ts
+++ index.ts
@@ -1,19 +1,5 @@
-interface IPerson {
-    name: string;
+type User  = {
+    firstName: string;
+    lastName: string;
     email: string;
-    sayMyName: (name: string) => void;
-}
-
-class Person implements IPerson {
-    name: string;
-    email: string;
-
-    constructor(name: string, email: string) {
-        this.name = name;
-        this.email = email;
-    }
-
-    sayMyName (name: string){
-        console.log(`say my name ${name}`)
-    }
 }
